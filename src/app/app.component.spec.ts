b666381874778972606e44e95ebb767dc5f9e913
Niyozbek Obidov <niyozbekdilshodovich@gmail.com>
2022-07-26 02:15:45 +0100

    - app name adjusted

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,6 +20,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('my-second-app app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('autograding-web-app app is running!');
   });
 });
