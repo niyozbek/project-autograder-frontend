3d163317578e862319c7cd4621fc0846ddfe7024
Bekhzod <alimov.behzod@gmail.com>
2025-12-03 11:20:04 +0500

    feat: Implement token expiration handling in auth interceptor to dispatch logout.

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,29 +1,42 @@
-import {HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from "@angular/common/http";
+import { HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Store } from "@ngrx/store";
-import { exhaustMap, map, take } from "rxjs/operators";
+import { catchError, exhaustMap, map, take } from "rxjs/operators";
+import { throwError } from "rxjs";
 import { AuthService } from "./auth.service";
-import * as fromApp from '../store/app.reducer'
+import * as fromApp from '../store/app.reducer';
+import * as AuthActions from './store/auth.actions';
 
 @Injectable()
-export class AuthInterceptorService implements HttpInterceptor{
-  constructor(private authService: AuthService, private store: Store<fromApp.AppState>){}
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor(
+    private authService: AuthService,
+    private store: Store<fromApp.AppState>
+  ) { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler){
+  intercept(req: HttpRequest<any>, next: HttpHandler) {
     return this.store.select('auth').pipe(
       take(1),
-      map(authState => {
-        return authState.user
-      }),
+      map(authState => authState.user),
       exhaustMap(user => {
-        if(!user){
-          return next.handle(req)
+        if (!user) {
+          return next.handle(req);
         }
+
         const modifiedReq = req.clone({
           headers: new HttpHeaders().set('Authorization', 'Bearer ' + user.token)
-        })
-        return next.handle(modifiedReq)
+        });
+
+        return next.handle(modifiedReq).pipe(
+          catchError(error => {
+            // Handle token expiration
+            if (error.status === 401 && error.error?.reason === 'TOKEN_EXPIRED') {
+              this.store.dispatch(new AuthActions.Logout());
+            }
+            return throwError(() => error);
+          })
+        );
       })
-    )
+    );
   }
 }
